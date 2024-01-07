8091c4dbd1a8f88aacdad35eb86605aed9795780
Your Name <you@example.com>
2024-01-07 19:27:25 +0800

    rm

--- 69958770-0c4f-4287-b974-679e0665a669.ts
+++ 69958770-0c4f-4287-b974-679e0665a669.ts
@@ -1,36 +1,67 @@
 export default {
-  entity: {
-    id: '69958770-0c4f-4287-b974-679e0665a669',
-    name: '揭发“四人帮”反党集团罪行材料摘编（一）',
-    internal: false,
-    official: false,
-    type: 'img',
-    author: '复旦大学政宣组',
-    files: new Array(32)
-      .fill(0)
-      .map(
-        (i, idx) =>
-          `https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/${
-            idx + 1
-          }.jpg`,
-      )
-      .join(','),
+  "entity": {
+    "id": "69958770-0c4f-4287-b974-679e0665a669",
+    "name": "揭发“四人帮”反党集团罪行材料摘编（一）",
+    "internal": false,
+    "official": false,
+    "type": "img",
+    "author": "复旦大学政宣组",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/12.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/13.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/14.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/15.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/16.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/17.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/18.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/19.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/20.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/21.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/22.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/23.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/24.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/25.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/26.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/27.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/28.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/29.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/30.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/31.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/69958770-0c4f-4287-b974-679e0665a669/32.jpg"
+    ]
   },
-  parser_option: {
-    page_limits: [],
-    ext: '',
-    articles: [
+  "parser_option": {
+    "page_limits": [],
+    "ext": "",
+    "articles": [
       {
-        title: '揭发“四人帮”反党集团罪行材料摘编（一）',
-        authors: ['复旦大学政宣组'],
-        page_start: 1,
-        page_end: 32,
-        dates: [{ year: 1976, month: 11 }],
-      },
+        "title": "揭发“四人帮”反党集团罪行材料摘编（一）",
+        "authors": [
+          "复旦大学政宣组"
+        ],
+        "page_start": 1,
+        "page_end": 32,
+        "dates": [
+          {
+            "year": 1976,
+            "month": 11
+          }
+        ]
+      }
     ],
-    ocr: undefined,
-    ocr_exceptions: {},
+    "ocr_exceptions": {}
   },
-  parser_id: 'automation',
-  path: '/archives15/69958770-0c4f-4287-b974-679e0665a669',
-};
+  "parser_id": "automation",
+  "path": "/archives15/69958770-0c4f-4287-b974-679e0665a669"
+}
--- 958040c5-a7f4-4b71-9581-e37fde18f05d.ts
+++ 958040c5-a7f4-4b71-9581-e37fde18f05d.ts
@@ -1,36 +1,45 @@
 export default {
-  entity: {
-    id: '958040c5-a7f4-4b71-9581-e37fde18f05d',
-    name: '山东省高级人民法院通告',
-    internal: false,
-    official: false,
-    type: 'img',
-    author: '山东省高级人民法院',
-    files: new Array(9)
-      .fill(0)
-      .map(
-        (i, idx) =>
-          `https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/958040c5-a7f4-4b71-9581-e37fde18f05d/${
-            idx + 1
-          }.jpg`,
-      )
-      .join(','),
+  "entity": {
+    "id": "958040c5-a7f4-4b71-9581-e37fde18f05d",
+    "name": "山东省高级人民法院通告",
+    "internal": false,
+    "official": false,
+    "type": "img",
+    "author": "山东省高级人民法院",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/958040c5-a7f4-4b71-9581-e37fde18f05d/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/958040c5-a7f4-4b71-9581-e37fde18f05d/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/958040c5-a7f4-4b71-9581-e37fde18f05d/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/958040c5-a7f4-4b71-9581-e37fde18f05d/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/958040c5-a7f4-4b71-9581-e37fde18f05d/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/958040c5-a7f4-4b71-9581-e37fde18f05d/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/958040c5-a7f4-4b71-9581-e37fde18f05d/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/958040c5-a7f4-4b71-9581-e37fde18f05d/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/958040c5-a7f4-4b71-9581-e37fde18f05d/9.jpg"
+    ]
   },
-  parser_option: {
-    page_limits: [],
-    ext: '',
-    articles: [
+  "parser_option": {
+    "page_limits": [],
+    "ext": "",
+    "articles": [
       {
-        title: '山东省高级人民法院通告',
-        authors: ['山东省高级人民法院'],
-        page_start: 1,
-        page_end: 9,
-        dates: [{ year: 1978, month: 11, day: 6 }],
-      },
+        "title": "山东省高级人民法院通告",
+        "authors": [
+          "山东省高级人民法院"
+        ],
+        "page_start": 1,
+        "page_end": 9,
+        "dates": [
+          {
+            "year": 1978,
+            "month": 11,
+            "day": 6
+          }
+        ]
+      }
     ],
-    ocr: undefined,
-    ocr_exceptions: {},
+    "ocr_exceptions": {}
   },
-  parser_id: 'automation',
-  path: '/archives15/958040c5-a7f4-4b71-9581-e37fde18f05d',
-};
+  "parser_id": "automation",
+  "path": "/archives15/958040c5-a7f4-4b71-9581-e37fde18f05d"
+}
