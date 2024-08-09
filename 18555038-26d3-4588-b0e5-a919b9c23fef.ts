8c73ac4c897e4e4460c1e00113c965f43f23b13e
Your Name <you@example.com>
2024-08-09 23:12:30 +0800

    1

---
+++ 18555038-26d3-4588-b0e5-a919b9c23fef.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "18555038-26d3-4588-b0e5-a919b9c23fef",
+    "name": "邱会作回忆录",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "邱会作",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/18555038-26d3-4588-b0e5-a919b9c23fef.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "邱会作回忆录",
+        "authors": [
+          "邱会作"
+        ],
+        "page_start": 1,
+        "page_end": 1023,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "18555038-26d3-4588-b0e5-a919b9c23fef.pdf",
+  "resource_type": "book",
+  "version": 2
+}
