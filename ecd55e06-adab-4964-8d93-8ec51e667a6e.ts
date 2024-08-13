fdefa7771e79a47dbea3f8712cacf17b820659b0
Your Name <you@example.com>
2024-08-13 20:26:08 +0800

    fix

--- ecd55e06-adab-4964-8d93-8ec51e667a6e.ts
+++ ecd55e06-adab-4964-8d93-8ec51e667a6e.ts
@@ -1,45 +1,45 @@
 export default {
-  "entity": {
-    "id": "ecd55e06-adab-4964-8d93-8ec51e667a6e",
-    "name": "一份配合“四人帮”篡党夺权的反革命宣言书——评《陈红兵给刘建勋同志的信》",
-    "internal": false,
-    "type": "imgs",
-    "official": false,
-    "author": "中共开封市委宣传部大批判组",
-    "files": [
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/ecd55e06-adab-4964-8d93-8ec51e667a6e/1.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/ecd55e06-adab-4964-8d93-8ec51e667a6e/2.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/ecd55e06-adab-4964-8d93-8ec51e667a6e/3.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/ecd55e06-adab-4964-8d93-8ec51e667a6e/4.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/ecd55e06-adab-4964-8d93-8ec51e667a6e/5.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/ecd55e06-adab-4964-8d93-8ec51e667a6e/6.jpg"
-    ]
-  },
-  "parser_option": {
-    "articles": [
-      {
-        "title": "一份配合“四人帮”篡党夺权的反革命宣言书——评《陈红兵给刘建勋同志的信》",
-        "authors": [
-          "中共开封市委宣传部大批判组"
-        ],
-        "page_start": 1,
-        "page_end": 6,
-        "dates": [
-          {
-            "year": 1976,
-            "month": 12
-          }
-        ]
-      }
+  entity: {
+    id: 'ecd55e06-adab-4964-8d93-8ec51e667a6e',
+    name: '一份配合“四人帮”篡党夺权的反革命宣言书——评《陈红兵给刘建勋同志的信》',
+    internal: false,
+    type: 'img',
+    official: false,
+    author: '中共开封市委宣传部大批判组',
+    files: [
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/ecd55e06-adab-4964-8d93-8ec51e667a6e/1.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/ecd55e06-adab-4964-8d93-8ec51e667a6e/2.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/ecd55e06-adab-4964-8d93-8ec51e667a6e/3.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/ecd55e06-adab-4964-8d93-8ec51e667a6e/4.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/ecd55e06-adab-4964-8d93-8ec51e667a6e/5.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives15/main/ecd55e06-adab-4964-8d93-8ec51e667a6e/6.jpg',
     ],
-    "ocr": {
-      "use_onnx": true,
-      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
-      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
-    }
   },
-  "parser_id": "automation",
-  "path": "ecd55e06-adab-4964-8d93-8ec51e667a6e",
-  "resource_type": "book",
-  "version": 2
-}
+  parser_option: {
+    articles: [
+      {
+        title: '一份配合“四人帮”篡党夺权的反革命宣言书——评《陈红兵给刘建勋同志的信》',
+        authors: [
+          '中共开封市委宣传部大批判组',
+        ],
+        page_start: 1,
+        page_end: 6,
+        dates: [
+          {
+            year: 1976,
+            month: 12,
+          },
+        ],
+      },
+    ],
+    ocr: {
+      use_onnx: true,
+      det_model_dir: './paddle/onnx/ch_PP-OCRv4_det_infer.onnx',
+      rec_model_dir: './paddle/onnx/ch_PP-OCRv4_rec_infer.onnx',
+    },
+  },
+  parser_id: 'automation',
+  path: 'ecd55e06-adab-4964-8d93-8ec51e667a6e',
+  resource_type: 'book',
+  version: 2,
+};
